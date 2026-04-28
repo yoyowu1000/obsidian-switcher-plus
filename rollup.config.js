40612b6dda85ab8ed4782ff3a70f0a23907e9113
Yuan-Yow Wu <36537674+yuanyowwu@users.noreply.github.com>
2026-04-28 18:31:19 +0200

    chore: rename fork plugin id to yoyo

--- rollup.config.js
+++ rollup.config.js
@@ -10,7 +10,7 @@ export default {
   input: 'src/main.ts',
   treeshake: false,
   output: {
-    dir: 'dist/yuanyowwu-switcher-plus',
+    dir: 'dist/yoyo-switcher-plus',
     sourcemap: isProd ? false : 'inline',
     sourcemapExcludeSources: isProd,
     format: 'cjs',
